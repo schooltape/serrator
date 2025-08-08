aa431cdb0d0c2f08a3f75045ea7e447a22c016c7
willow <42willow@pm.me>
2025-08-08 21:14:47 +1000

    refactor: extract tile group scraper logic to utils module

--- src/homepage/index.ts
+++ src/homepage/index.ts
@@ -1,5 +1,5 @@
 import { JSDOM } from "jsdom";
-import { getUrlFromCss } from "../utils";
+import { getTileGroups, getUrlFromCss } from "../utils";
 import type { SchoolboxHomepage, Tile, TileGroup } from "../types";
 
 /**
@@ -9,24 +9,7 @@ export function getHomepage(html: string): SchoolboxHomepage {
   const dom = new JSDOM(html);
   const document = dom.window.document;
 
-  const tileGroups: TileGroup[] = Array.from(
-    document.querySelectorAll(".tileList"),
-  ).map((tileGroup) => {
-    const tiles: Tile[] = Array.from(
-      tileGroup.querySelectorAll<HTMLLIElement>(
-        ".Schoolbox_Tile_Component_HomepageTileController li.tile",
-      ),
-    ).map((tile) => {
-      return {
-        title: tile.querySelector(".title")?.textContent?.trim() || "",
-        link: tile.querySelector("a")?.getAttribute("href") || "",
-        imageUrl: getUrlFromCss(
-          dom.window.getComputedStyle(tile).backgroundImage,
-        ),
-      };
-    });
-    return tiles;
-  });
+  const tileGroups: TileGroup[] = getTileGroups(dom);
 
   return {
     title: document.querySelector("h1")?.textContent?.trim() || "",
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,30 @@
-import { JWT } from "../tests/env";
+import { JWT } from "./env";
+import { JSDOM } from "jsdom";
+import type { Tile, TileGroup } from "./types";
 
 export function getUrlFromCss(css: string) {
   return css.match(/"(.*?)"/)?.[1] ?? "";
+}
+
+export function getTileGroups(dom: JSDOM): TileGroup[] {
+  const document = dom.window.document;
+
+  return Array.from(document.querySelectorAll(".tileList")).map((tileGroup) => {
+    const tiles: Tile[] = Array.from(
+      tileGroup.querySelectorAll<HTMLLIElement>(
+        ".Schoolbox_Tile_Component_HomepageTileController li.tile",
+      ),
+    ).map((tile) => {
+      return {
+        title: tile.querySelector(".title")?.textContent?.trim() || "",
+        link: tile.querySelector("a")?.getAttribute("href") || "",
+        imageUrl: getUrlFromCss(
+          dom.window.getComputedStyle(tile).backgroundImage,
+        ),
+      };
+    });
+    return tiles;
+  });
 }
 
 export async function makeAuthRequest<T>(
