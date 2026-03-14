0bb08a44342a29a62230c8ef858b53bf9a6b2f53
willow <42willow@pm.me>
2026-03-14 21:38:07 +1100

    fix: typo

--- src/modules/notifications/types.ts
+++ src/modules/notifications/types.ts
@@ -1,7 +1,7 @@
 export interface SchoolboxNotification {
   link: string;
   imgUrl?: string;
-  userIds: string[]; // can have mulitple users, e.g. squashed posts on a social stream
+  userIds: string[]; // can have multiple users, e.g. squashed posts on a social stream
   body: string;
   unread: boolean;
   date: Date;
